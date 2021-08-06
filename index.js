937f8ce68bb021f2836746a4af3340c753668cfc
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-08-06 15:19:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+document.addEventListener("DOMContentLoaded", () => {
+  return submitData("Test1", "test1@foobar.com");
+})
+
+function submitData(userName, userEmail) {
+  const formData = {
+    name: userName,
+    email: userEmail
+  }
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+
+  return fetch("http://localhost:3000/users", configObj)
+           .then(response => response.json())
+           .then(result => console.log(result))
+}
