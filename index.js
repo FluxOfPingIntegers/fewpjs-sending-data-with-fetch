e64c1b9704fd3d3007c3f800817ea359072ccf4b
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-08-07 09:03:14 -0400

    commit tree 4387e07e2678784081bb01df0e2cc8cf496cc1f6

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 // Add your code here
-document.addEventListener("DOMContentLoaded", () => {
-  return submitData("Test1", "test1@foobar.com");
-})
 
-function submitData(userName, userEmail) {
-  const formData = {
-    name: userName,
-    email: userEmail
+
+  function submitData(userName, userEmail) {
+    console.log(`${userName} has an email of ${userEmail}`)
+  
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({name: userName, email: userEmail})
+    }
+  
+    return fetch("http://localhost:3000/users", configObj)
+             .then(response => response.json())
+             .then(result => {
+               document.body.innerHTML += `<p>${JSON.stringify(result["id"])}</p>`;
+             })
+             .catch(function(error) {
+               alert(`${error.message}`);
+               console.log(`${error.message}`);
+               document.body.innerHTML += `<p>${error.message}</p>`;
+             })
   }
-
-  const configObj = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(formData)
-  }
-
-  return fetch("http://localhost:3000/users", configObj)
-           .then(response => response.json())
-           .then(result => console.log(result))
-}
